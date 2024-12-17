f7c88863a4004041d444fef20e09a66aef6a94df
Alejandra Murillo <alemurillo104@gmail.com>
2024-12-17 12:31:10 -0400

    uploading repository logic to retrieve tasks list from db

---
+++ packages/home_module/lib/src/data/repositories/task.repository.dart
@@ -0,0 +1,21 @@
+import 'package:common_dependency_module/common_dependency_module.dart';
+import '../models/task.model.dart';
+import '../../domain/mappers/task.enum.mapper.dart';
+
+class TaskRepository {
+  final _collection = FirebaseFirestore.instance.collection('tasks');
+
+  Future<List<TaskModel>> getTasks() async {
+    var snapshot = await _collection.get();
+    var accounts = snapshot.docs.map((doc) {
+      var item = doc.data();
+      return TaskModel(
+        name: item['name'],
+        description: item['description'],
+        status: TaskStatusMapper.toEnum(item['status']),
+      );
+    }).toList();
+
+    return accounts;
+  }
+}
