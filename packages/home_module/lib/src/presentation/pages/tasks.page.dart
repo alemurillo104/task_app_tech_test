1e490f58fb439330a27eb1b1cb1744aa2e2d8571
Alejandra Murillo <alemurillo104@gmail.com>
2024-12-17 17:03:03 -0400

    updating logic to filter tasks by status in task page

--- packages/home_module/lib/src/presentation/pages/tasks.page.dart
+++ packages/home_module/lib/src/presentation/pages/tasks.page.dart
@@ -6,7 +6,9 @@ import '../blocs/addTask/add_task_bloc.dart';
 import '../../../navigation/navigation.dart';
 import '../blocs/updateTask/update_task_bloc.dart';
 import '../blocs/deleteTask/delete_task_bloc.dart';
+import '../../domain/mappers/task.enum.mapper.dart';
 import '../widgets/add_task_bottom_sheet.widget.dart';
+import '../widgets/filter_task_bottom_sheet.widget.dart';
 
 class TasksPage extends StatefulWidget {
   const TasksPage({super.key});
@@ -19,13 +21,15 @@ class _TasksPageState extends State<TasksPage> {
   final deleteTaskBloc = Modular.get<DeleteTaskBloc>();
   final updateTaskBloc = Modular.get<UpdateTaskBloc>();
   final addTaskBloc = Modular.get<AddTaskBloc>();
+  final tasksBloc = Modular.get<TasksBloc>();
+  String? taskStatusSelected;
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (context) => Modular.get<TasksBloc>()
+          create: (context) => tasksBloc
             ..add(
               const TasksFetched(),
             ),
@@ -119,6 +123,28 @@ class _TasksPageState extends State<TasksPage> {
                                     );
                                   },
                                 );
+                              case TasksStatus.filterMode:
+                                return ListView.separated(
+                                  separatorBuilder: (context, index) {
+                                    return const SizedBox(height: 10);
+                                  },
+                                  itemCount: state.filteredtasks.length,
+                                  itemBuilder: (context, index) {
+                                    return TaskItemWidget(
+                                      task: state.filteredtasks[index],
+                                      onTap: () {
+                                        Modular.to.pushNamed(
+                                          HomeDestination.taskDetail,
+                                          arguments: {
+                                            'task': state.filteredtasks[index],
+                                            'deleteTaskBloc': deleteTaskBloc,
+                                            'updateTaskBloc': updateTaskBloc,
+                                          },
+                                        );
+                                      },
+                                    );
+                                  },
+                                );
                               case TasksStatus.failure:
                                 return const Text('Error');
                               default:
@@ -127,6 +153,49 @@ class _TasksPageState extends State<TasksPage> {
                                 );
                             }
                           },
+                        ),
+                      ),
+                      InkWell(
+                        borderRadius: BorderRadius.circular(20),
+                        onTap: () {
+                          showModalBottomSheet(
+                            context: context,
+                            builder: (_) {
+                              return FilterTaskBottomSheetWidget(
+                                currentTaskStatusSelected: taskStatusSelected,
+                                onFilterButtonPressed: (status) {
+                                  logger.d(status);
+                                  tasksBloc.add(TasksFiltered(status));
+                                  taskStatusSelected =
+                                      TaskStatusMapper.fromEnumtoLiteralString(
+                                    status,
+                                  );
+                                  setState(() {});
+                                },
+                                onClearFilterPressed: () {
+                                  logger.d('clear filters');
+                                  taskStatusSelected = null;
+                                  setState(() {});
+                                  tasksBloc.add(const ResetTasksList());
+                                },
+                              );
+                            },
+                          );
+                        },
+                        child: Container(
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.blue.shade100,
+                          ),
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.filter_alt_outlined),
+                              SizedBox(width: 8),
+                              Text('Filtrar tareas'),
+                            ],
+                          ),
                         ),
                       ),
                       const SizedBox(height: 12),
@@ -138,7 +207,6 @@ class _TasksPageState extends State<TasksPage> {
                             builder: (context) {
                               return AddTaskBottomSheetWidget(
                                 addTaskBloc: addTaskBloc,
-                                
                               );
                             },
                           );
