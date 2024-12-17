3b12f9eb6256c56674202b7642642f2a7e07088c
Alejandra Murillo <alemurillo104@gmail.com>
2024-12-17 14:14:06 -0400

    updating task model

--- packages/home_module/lib/src/data/models/task.model.dart
+++ packages/home_module/lib/src/data/models/task.model.dart
@@ -1,7 +1,8 @@
 import '../../domain/entities/task.entity.dart';
 
 class TaskModel extends Task {
-  const TaskModel({
+  TaskModel({
+    super.id,
     required super.name,
     required super.description,
     required super.status,
--- packages/home_module/lib/src/domain/entities/task.entity.dart
+++ packages/home_module/lib/src/domain/entities/task.entity.dart
@@ -2,16 +2,18 @@ import 'package:common_dependency_module/common_dependency_module.dart';
 import '../enums/task_status.enum.dart';
 
 class Task extends Equatable {
+  String? id;
   final String name;
   final String description;
   final TaskStatus status;
 
-  const Task({
+  Task({
+    this.id,
     required this.name,
     required this.description,
     required this.status,
   });
 
   @override
-  List<Object?> get props => [name, description, status];
+  List<Object?> get props => [id, name, description, status];
 }
