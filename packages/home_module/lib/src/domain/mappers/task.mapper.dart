1b11d95aa73c21c42adad3866f457259d2469063
Alejandra Murillo <alemurillo104@gmail.com>
2024-12-17 14:16:04 -0400

    adding delete task method

--- packages/home_module/lib/src/domain/mappers/task.mapper.dart
+++ packages/home_module/lib/src/domain/mappers/task.mapper.dart
@@ -4,6 +4,7 @@ import '../../data/models/task.model.dart';
 class TaskMapper {
   static Task mapToEntity(TaskModel model) {
     return Task(
+      id: model.id,
       name: model.name,
       description: model.description,
       status: model.status,
