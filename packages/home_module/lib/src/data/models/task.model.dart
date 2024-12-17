8dcaf2b4093ac8ed47e89649b84423856be11c23
Alejandra Murillo <alemurillo104@gmail.com>
2024-12-17 16:10:00 -0400

    updating task model

--- packages/home_module/lib/src/data/models/task.model.dart
+++ packages/home_module/lib/src/data/models/task.model.dart
@@ -1,4 +1,5 @@
 import '../../domain/entities/task.entity.dart';
+import '../../domain/mappers/task.enum.mapper.dart';
 
 class TaskModel extends Task {
   TaskModel({
@@ -7,4 +8,21 @@ class TaskModel extends Task {
     required super.description,
     required super.status,
   });
+
+  factory TaskModel.fromJson(Map<String, dynamic> json) => TaskModel(
+        id: json['id'],
+        name: json['name'],
+        description: json['description'],
+        status: TaskStatusMapper.toEnum(json['status']),
+      );
+
+  Map<String, dynamic> toJson() {
+    var task = {
+      'name': name,
+      'description': description,
+      'status': TaskStatusMapper.fromEnumtoString(status),
+    };
+    if (id != null) task['id'] = id!;
+    return task;
+  }
 }
