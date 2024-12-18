f95ab2e33638937483f0025c7be41a2f4028981e
Alejandra Murillo <alemurillo104@gmail.com>
2024-12-17 23:43:13 -0400

    adding string extension

--- packages/common_module/lib/common_module.dart
+++ packages/common_module/lib/common_module.dart
@@ -4,6 +4,7 @@ export 'src/components/dialogs/show_dialog.widget.dart';
 export 'src/components/inputs/dropdown_input.widget.dart';
 export 'src/components/overlay/loading.overlay.widget.dart';
 export 'src/components/inputs/custom_textformfield.widget.dart';
+export 'src/components/extensions/text.capitalize.extension.dart';
 
 /// A Calculator.
 class Calculator {
